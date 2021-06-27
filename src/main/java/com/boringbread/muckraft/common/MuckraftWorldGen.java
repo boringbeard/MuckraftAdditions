2c15f9fcd8687cd3fdc4908914e1d7a54993f7f3
boringbread <79241012+boringbeard@users.noreply.github.com>
2021-06-27 11:46:17 -0500

    made some code look nicer and renamed preinit to preinitcommon in MuckraftWorldGen, got started on the worldtype file too.

--- src/main/java/com/boringbread/muckraft/common/MuckraftWorldGen.java
+++ src/main/java/com/boringbread/muckraft/common/MuckraftWorldGen.java
@@ -6,7 +6,7 @@ import net.minecraftforge.common.DimensionManager;
 public class MuckraftWorldGen {
             public static final DimensionType STAGE_ONE = DimensionType.register("stageone", "_stageone", 69, MuckraftWorldProvider.class, true);
 
-            public static void preInit()
+            public static void preInitCommon()
             {
                 DimensionManager.registerDimension(69, STAGE_ONE);
             }
