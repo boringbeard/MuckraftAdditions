43cd3aa743d37f5f0e43fc9c6b47b9fdc07017e6
boringbread <79241012+boringbeard@users.noreply.github.com>
2021-06-30 14:52:48 -0500

    made the teleporter a bit more functional.  It now teleports the player to a 3x3 clear square on the surface

--- src/main/java/com/boringbread/muckraft/world/MuckTeleporter.java
+++ src/main/java/com/boringbread/muckraft/world/MuckTeleporter.java
@@ -1,6 +1,8 @@
 package com.boringbread.muckraft.world;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ITeleporter;
@@ -14,28 +16,73 @@ public class MuckTeleporter implements ITeleporter {
         int y = (int)Math.floor(entity.posY);
         int z = (int)Math.floor(entity.posZ);
 
-        //for (int changeY = -1; changeY < 2; changeY++)
-        //{
-        //    world.setBlockState(new BlockPos(x, y + changeY, z), (changeY == -1) ? ModBlocks.PORTAL_STAGE_ONE.getDefaultState() : Blocks.AIR.getDefaultState());
-        //}
-        System.out.println(world.isChunkGeneratedAt(x, z));
         BlockPos pos = new BlockPos(x, y, z);
-        BlockPos top = world.getHeight(pos);
-        entity.setLocationAndAngles(pos.getX(), top.getY(), pos.getZ(), yaw, 0.0F);
+        BlockPos newPos = findAcceptableLocation(300, pos, world);
 
+        entity.setLocationAndAngles(newPos.getX(), newPos.getY(), newPos.getZ(), yaw, 0.0F);
         entity.motionX = 0;
         entity.motionY = 0;
         entity.motionZ = 0;
     }
 
     @Nullable
-    private BlockPos findAcceptableLocation(int range, BlockPos pos, Entity entity)
+    private BlockPos findAcceptableLocation(int range, BlockPos pos, World world)
     {
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
+
+        for(int r = 0; r <= range; r += range/6)
+        {
+            int checksPerRing = 8;
+
+            for(int i = 0; i <= checksPerRing; i++)
+            {
+                double tau = 2 * Math.PI;
+                int changeX = (int) Math.round(r * Math.cos(i * tau / checksPerRing));
+                int changeZ = (int) Math.round(r * Math.sin(i * tau / checksPerRing));
+                int x1 = x + changeX;
+                int z1 = z + changeZ;
+                int y1 = getGoodHeight(new BlockPos(x1, y, z1), world);
+
+                System.out.println(changeX + ", " + changeZ);
+
+                boolean isAcceptableLocation = y1 != -1;
+
+                if (isAcceptableLocation) return new BlockPos(x1, y1, z1);
+            }
+        }
         return null;
     }
 
-    private boolean isAcceptableLocation(BlockPos pos, World world)
+    private int getGoodHeight(BlockPos pos, World world)
     {
-        return false;
+        int x = pos.getX();
+        int z = pos.getZ();
+        int y1 = -1;
+
+        for(int y = 63; y < 130; y++)
+        {
+            int counter = 0;
+
+            for(int x1 = x - 1; x1 < x + 2; x1++)
+            {
+                for(int z1 = z - 1; z1 < z + 2; z1++)
+                {
+                    BlockPos checkPos = new BlockPos(x1, y, z1);
+                    IBlockState blockState = world.getBlockState(checkPos.add(0,-1, 0));
+
+                    if (world.getLight(checkPos) == 15 && blockState.isOpaqueCube()) counter++;
+                }
+            }
+
+            if(counter == 9)
+            {
+               y1 = y;
+               break;
+            }
+        }
+
+        return y1;
     }
 }
