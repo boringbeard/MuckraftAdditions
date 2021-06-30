818e0909a58e30ab6b84b123e05733fa24430a05
boringbread <79241012+boringbeard@users.noreply.github.com>
2021-06-29 23:26:38 -0500

    added some currently unused methods to the teleporter for finding portal spots in the future

--- src/main/java/com/boringbread/muckraft/world/MuckTeleporter.java
+++ src/main/java/com/boringbread/muckraft/world/MuckTeleporter.java
@@ -1,15 +1,41 @@
 package com.boringbread.muckraft.world;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ITeleporter;
+
+import javax.annotation.Nullable;
 
 public class MuckTeleporter implements ITeleporter {
     @Override
     public void placeEntity(World world, Entity entity, float yaw) {
-        entity.setLocationAndAngles(0, 100, 0, yaw, 0.0F);
+        int x = (int)Math.floor(entity.posX);
+        int y = (int)Math.floor(entity.posY);
+        int z = (int)Math.floor(entity.posZ);
+
+        //for (int changeY = -1; changeY < 2; changeY++)
+        //{
+        //    world.setBlockState(new BlockPos(x, y + changeY, z), (changeY == -1) ? ModBlocks.PORTAL_STAGE_ONE.getDefaultState() : Blocks.AIR.getDefaultState());
+        //}
+        System.out.println(world.isChunkGeneratedAt(x, z));
+        BlockPos pos = new BlockPos(x, y, z);
+        BlockPos top = world.getHeight(pos);
+        entity.setLocationAndAngles(pos.getX(), top.getY(), pos.getZ(), yaw, 0.0F);
+
         entity.motionX = 0;
         entity.motionY = 0;
         entity.motionZ = 0;
+    }
+
+    @Nullable
+    private BlockPos findAcceptableLocation(int range, BlockPos pos, Entity entity)
+    {
+        return null;
+    }
+
+    private boolean isAcceptableLocation(BlockPos pos, World world)
+    {
+        return false;
     }
 }
