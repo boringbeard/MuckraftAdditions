5d362fc11cb2beb3e020f2a9e83e0a02184a70da
boringbread <79241012+boringbeard@users.noreply.github.com>
2021-07-04 23:22:08 -0500

    added biome provider

--- src/main/java/com/boringbread/muckraft/world/MuckraftWorldProvider.java
+++ src/main/java/com/boringbread/muckraft/world/MuckraftWorldProvider.java
@@ -1,11 +1,20 @@
 package com.boringbread.muckraft.world;
 
 import com.boringbread.muckraft.init.MuckraftWorldGen;
+import com.boringbread.muckraft.world.biome.StageOneBiomeProvider;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.WorldProvider;
 import net.minecraft.world.gen.IChunkGenerator;
 
-public class MuckraftWorldProvider extends WorldProvider {
+public class MuckraftWorldProvider extends WorldProvider
+{
+    @Override
+    protected void init()
+    {
+        this.hasSkyLight = true;
+        this.biomeProvider = new StageOneBiomeProvider();
+    }
+
     @Override
     public DimensionType getDimensionType() {
         return MuckraftWorldGen.STAGE_ONE;
---
+++ src/main/java/com/boringbread/muckraft/world/biome/StageOneBiomeProvider.java
@@ -0,0 +1,12 @@
+package com.boringbread.muckraft.world.biome;
+
+import net.minecraft.init.Biomes;
+import net.minecraft.world.biome.BiomeProviderSingle;
+
+public class StageOneBiomeProvider extends BiomeProviderSingle
+{
+    public StageOneBiomeProvider()
+    {
+        super(Biomes.PLAINS);
+    }
+}
