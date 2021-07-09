197ebd98bd677c5b9bd67e6e2c6411f2fa9852c3
boringbread <79241012+boringbeard@users.noreply.github.com>
2021-07-08 21:27:43 -0500

    trying to find out how which layers do what is too much work.  For now the first dimension is just the overworld minus rare biomes

--- src/main/java/com/boringbread/muckraft/world/biome/StageOneBiomeProvider.java
+++ src/main/java/com/boringbread/muckraft/world/biome/StageOneBiomeProvider.java
@@ -233,47 +233,46 @@ public class StageOneBiomeProvider extends BiomeProvider
         genlayeraddisland1 = new GenLayerAddIsland(50L, genlayeraddisland1);
         genlayeraddisland1 = new GenLayerAddIsland(70L, genlayeraddisland1);
         GenLayer genlayerremovetoomuchocean = new GenLayerRemoveTooMuchOcean(2L, genlayeraddisland1);
-        GenLayer genlayeredge = new GenLayerEdge(2L, genlayerremovetoomuchocean, GenLayerEdge.Mode.HEAT_ICE);
+        GenLayer genlayeraddsnow = new GenLayerAddSnow(2L, genlayerremovetoomuchocean);
+        GenLayer genlayeraddisland2 = new GenLayerAddIsland(3L, genlayeraddsnow);
+        GenLayer genlayeredge = new GenLayerEdge(2L, genlayeraddisland2, GenLayerEdge.Mode.COOL_WARM);
+        genlayeredge = new GenLayerEdge(2L, genlayeredge, GenLayerEdge.Mode.HEAT_ICE);
         genlayeredge = new GenLayerEdge(3L, genlayeredge, GenLayerEdge.Mode.SPECIAL);
         GenLayer genlayerzoom1 = new GenLayerZoom(2002L, genlayeredge);
         genlayerzoom1 = new GenLayerZoom(2003L, genlayerzoom1);
         GenLayer genlayeraddisland3 = new GenLayerAddIsland(4L, genlayerzoom1);
-        GenLayer genlayerdeepocean = new GenLayerDeepOcean(4L, genlayeraddisland3);
+        GenLayer genlayeraddmushroomisland = new GenLayerAddMushroomIsland(5L, genlayeraddisland3);
+        GenLayer genlayerdeepocean = new GenLayerDeepOcean(4L, genlayeraddmushroomisland);
         GenLayer genlayer4 = GenLayerZoom.magnify(1000L, genlayerdeepocean, 0);
-        int biomeSize = 4;
-        int riverSize = biomeSize;
-
-        if (chunkGeneratorSettings != null)
-        {
-            biomeSize = chunkGeneratorSettings.biomeSize;
-            riverSize = chunkGeneratorSettings.riverSize;
-        }
+        int i = 4;
+        int j = i;
 
         GenLayer lvt_7_1_ = GenLayerZoom.magnify(1000L, genlayer4, 0);
         GenLayer genlayerriverinit = new GenLayerRiverInit(100L, lvt_7_1_);
         GenLayer genlayerbiomeedge = worldType.getBiomeLayer(seed, genlayer4, chunkGeneratorSettings);
         GenLayer lvt_9_1_ = GenLayerZoom.magnify(1000L, genlayerriverinit, 2);
+        GenLayer genlayerhills = new GenLayerHills(1000L, genlayerbiomeedge, lvt_9_1_);
         GenLayer genlayer5 = GenLayerZoom.magnify(1000L, genlayerriverinit, 2);
-        genlayer5 = GenLayerZoom.magnify(1000L, genlayer5, riverSize);
+        genlayer5 = GenLayerZoom.magnify(1000L, genlayer5, j);
         GenLayer genlayerriver = new GenLayerRiver(1L, genlayer5);
         GenLayer genlayersmooth = new GenLayerSmooth(1000L, genlayerriver);
 
-        for (int k = 0; k < biomeSize; ++k)
+        for (int k = 0; k < i; ++k)
         {
-            genlayer = new GenLayerZoom((long)(1000 + k), genlayer);
+            genlayerhills = new GenLayerZoom(1000 + k, genlayerhills);
 
             if (k == 0)
             {
-                genlayer = new GenLayerAddIsland(3L, genlayer);
+                genlayerhills = new GenLayerAddIsland(3L, genlayerhills);
             }
 
-            if (k == 1 || biomeSize == 1)
+            if (k == 1 || i == 1)
             {
-                genlayer = new GenLayerShore(1000L, genlayer);
+                genlayerhills = new GenLayerShore(1000L, genlayerhills);
             }
         }
 
-        GenLayer genlayersmooth1 = new GenLayerSmooth(1000L, genlayer);
+        GenLayer genlayersmooth1 = new GenLayerSmooth(1000L, genlayerhills);
         GenLayer genlayerrivermix = new GenLayerRiverMix(100L, genlayersmooth1, genlayersmooth);
         GenLayer genlayer3 = new GenLayerVoronoiZoom(10L, genlayerrivermix);
         genlayerrivermix.initWorldGenSeed(seed);
