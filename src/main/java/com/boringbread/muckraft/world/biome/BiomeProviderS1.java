0ea3e830c6c0f82bdc1885d9a048b7287867b878
boringbread <79241012+boringbeard@users.noreply.github.com>
2021-07-09 16:19:22 -0500

    found a better way to remove biomes and such

--- src/main/java/com/boringbread/muckraft/world/biome/BiomeProviderS1.java
+++ src/main/java/com/boringbread/muckraft/world/biome/BiomeProviderS1.java
@@ -7,14 +7,13 @@ import net.minecraft.init.Biomes;
 import net.minecraft.util.ReportedException;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.WorldType;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.BiomeCache;
-import net.minecraft.world.biome.BiomeProvider;
+import net.minecraft.world.biome.*;
 import net.minecraft.world.gen.ChunkGeneratorSettings;
 import net.minecraft.world.gen.layer.*;
 import net.minecraft.world.storage.WorldInfo;
 
 import javax.annotation.Nullable;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -25,11 +24,27 @@ public class BiomeProviderS1 extends BiomeProvider
     private GenLayer biomeIndexLayer;
     private final BiomeCache biomeCache;
     private final List<Biome> biomesToSpawnIn;
+    private final BiomeReplacer[] biomeReplacers = new BiomeReplacer[]
+            {
+                    new BiomeReplacer(Biomes.FOREST_HILLS, Biomes.FOREST),
+                    new BiomeReplacer(Biomes.BIRCH_FOREST_HILLS, Biomes.BIRCH_FOREST),
+                    new BiomeReplacer(Biomes.COLD_TAIGA_HILLS, Biomes.COLD_TAIGA),
+                    new BiomeReplacer(Biomes.DESERT_HILLS, Biomes.DESERT),
+                    new BiomeReplacer(Biomes.ICE_MOUNTAINS, Biomes.ICE_PLAINS),
+                    new BiomeReplacer(Biomes.JUNGLE, Biomes.PLAINS),
+                    new BiomeReplacer(Biomes.JUNGLE_EDGE, Biomes.PLAINS),
+                    new BiomeReplacer(Biomes.JUNGLE_HILLS, Biomes.PLAINS),
+                    new BiomeReplacer(Biomes.REDWOOD_TAIGA, Biomes.TAIGA),
+                    new BiomeReplacer(Biomes.REDWOOD_TAIGA_HILLS, Biomes.TAIGA),
+                    new BiomeReplacer(Biomes.MUSHROOM_ISLAND, Biomes.PLAINS),
+                    new BiomeReplacer(Biomes.MUSHROOM_ISLAND_SHORE, Biomes.BEACH),
+                    new BiomeReplacer(Biomes.SAVANNA_PLATEAU, Biomes.SAVANNA),
+                    new BiomeReplacer(Biomes.MUTATED_SAVANNA_ROCK, Biomes.SAVANNA),
+                    new BiomeReplacer(Biomes.MUTATED_SAVANNA, Biomes.SAVANNA)
+            };
 
     protected BiomeProviderS1()
     {
-        allowedBiomes.clear();
-        allowedBiomes.add(Biomes.PLAINS);
         this.biomeCache = new BiomeCache(this);
         this.biomesToSpawnIn = Lists.newArrayList(allowedBiomes);
     }
@@ -37,7 +52,7 @@ public class BiomeProviderS1 extends BiomeProvider
     private BiomeProviderS1(long seed, WorldType worldTypeIn)
     {
         this();
-        GenLayer[] agenlayer = initializeAllBiomeGenerators(seed, worldTypeIn, this.settings);
+        GenLayer[] agenlayer = GenLayer.initializeAllBiomeGenerators(seed, worldTypeIn, this.settings);
         this.genBiomes = agenlayer[0];
         this.biomeIndexLayer = agenlayer[1];
     }
@@ -83,6 +98,15 @@ public class BiomeProviderS1 extends BiomeProvider
             for (int i = 0; i < width * height; ++i)
             {
                 biomes[i] = Biome.getBiome(aint[i], Biomes.DEFAULT);
+            }
+
+            for (int i = 0; i < biomes.length; i++) {
+                if(biomes[i] instanceof BiomeJungle) biomes[i] = Biomes.MUTATED_SAVANNA;
+                if(biomes[i] instanceof BiomeHills) biomes[i] = Biomes.FOREST;
+                if(biomes[i] instanceof BiomeMesa) biomes[i] = Biomes.DESERT;
+                for (BiomeReplacer biomeReplacer: biomeReplacers) {
+                    if(biomeReplacer.toBeReplaced == biomes[i]) biomes[i] = biomeReplacer.replacement;
+                }
             }
 
             return biomes;
@@ -201,83 +225,16 @@ public class BiomeProviderS1 extends BiomeProvider
         return blockpos;
     }
 
-    public void cleanupCache()
+    private static class BiomeReplacer
     {
-        this.biomeCache.cleanupCache();
-    }
+        private final Biome toBeReplaced;
+        private final Biome replacement;
 
-    public GenLayer[] getModdedBiomeGenerators(WorldType worldType, long seed, GenLayer[] original)
-    {
-        net.minecraftforge.event.terraingen.WorldTypeEvent.InitBiomeGens event = new net.minecraftforge.event.terraingen.WorldTypeEvent.InitBiomeGens(worldType, seed, original);
-        net.minecraftforge.common.MinecraftForge.TERRAIN_GEN_BUS.post(event);
-        return event.getNewBiomeGens();
-    }
-
-    public boolean isFixedBiome()
-    {
-        return this.settings != null && this.settings.fixedBiome >= 0;
-    }
-
-    public Biome getFixedBiome()
-    {
-        return this.settings != null && this.settings.fixedBiome >= 0 ? Biome.getBiomeForId(this.settings.fixedBiome) : null;
-    }
-
-    public static GenLayer[] initializeAllBiomeGenerators(long seed, WorldType worldType, ChunkGeneratorSettings chunkGeneratorSettings)
-    {
-        GenLayer genlayer = new GenLayerIsland(1L);
-        genlayer = new GenLayerFuzzyZoom(2000L, genlayer);
-        GenLayer genlayeraddisland = new GenLayerAddIsland(1L, genlayer);
-        GenLayer genlayerzoom = new GenLayerZoom(2001L, genlayeraddisland);
-        GenLayer genlayeraddisland1 = new GenLayerAddIsland(2L, genlayerzoom);
-        genlayeraddisland1 = new GenLayerAddIsland(50L, genlayeraddisland1);
-        genlayeraddisland1 = new GenLayerAddIsland(70L, genlayeraddisland1);
-        GenLayer genlayerremovetoomuchocean = new GenLayerRemoveTooMuchOcean(2L, genlayeraddisland1);
-        GenLayer genlayeraddsnow = new GenLayerAddSnow(2L, genlayerremovetoomuchocean);
-        GenLayer genlayeraddisland2 = new GenLayerAddIsland(3L, genlayeraddsnow);
-        GenLayer genlayeredge = new GenLayerEdge(2L, genlayeraddisland2, GenLayerEdge.Mode.COOL_WARM);
-        genlayeredge = new GenLayerEdge(2L, genlayeredge, GenLayerEdge.Mode.HEAT_ICE);
-        genlayeredge = new GenLayerEdge(3L, genlayeredge, GenLayerEdge.Mode.SPECIAL);
-        GenLayer genlayerzoom1 = new GenLayerZoom(2002L, genlayeredge);
-        genlayerzoom1 = new GenLayerZoom(2003L, genlayerzoom1);
-        GenLayer genlayeraddisland3 = new GenLayerAddIsland(4L, genlayerzoom1);
-        GenLayer genlayeraddmushroomisland = new GenLayerAddMushroomIsland(5L, genlayeraddisland3);
-        GenLayer genlayerdeepocean = new GenLayerDeepOcean(4L, genlayeraddmushroomisland);
-        GenLayer genlayer4 = GenLayerZoom.magnify(1000L, genlayerdeepocean, 0);
-        int i = 4;
-        int j = i;
-
-        GenLayer lvt_7_1_ = GenLayerZoom.magnify(1000L, genlayer4, 0);
-        GenLayer genlayerriverinit = new GenLayerRiverInit(100L, lvt_7_1_);
-        GenLayer genlayerbiomeedge = worldType.getBiomeLayer(seed, genlayer4, chunkGeneratorSettings);
-        GenLayer lvt_9_1_ = GenLayerZoom.magnify(1000L, genlayerriverinit, 2);
-        GenLayer genlayerhills = new GenLayerHills(1000L, genlayerbiomeedge, lvt_9_1_);
-        GenLayer genlayer5 = GenLayerZoom.magnify(1000L, genlayerriverinit, 2);
-        genlayer5 = GenLayerZoom.magnify(1000L, genlayer5, j);
-        GenLayer genlayerriver = new GenLayerRiver(1L, genlayer5);
-        GenLayer genlayersmooth = new GenLayerSmooth(1000L, genlayerriver);
-
-        for (int k = 0; k < i; ++k)
+        private BiomeReplacer(Biome toBeReplaced, Biome replacement)
         {
-            genlayerhills = new GenLayerZoom(1000 + k, genlayerhills);
-
-            if (k == 0)
-            {
-                genlayerhills = new GenLayerAddIsland(3L, genlayerhills);
-            }
-
-            if (k == 1 || i == 1)
-            {
-                genlayerhills = new GenLayerShore(1000L, genlayerhills);
-            }
+            this.toBeReplaced = toBeReplaced;
+            this.replacement = replacement;
         }
-
-        GenLayer genlayersmooth1 = new GenLayerSmooth(1000L, genlayerhills);
-        GenLayer genlayerrivermix = new GenLayerRiverMix(100L, genlayersmooth1, genlayersmooth);
-        GenLayer genlayer3 = new GenLayerVoronoiZoom(10L, genlayerrivermix);
-        genlayerrivermix.initWorldGenSeed(seed);
-        genlayer3.initWorldGenSeed(seed);
-        return new GenLayer[] {genlayerrivermix, genlayer3, genlayerrivermix};
     }
 }
 
