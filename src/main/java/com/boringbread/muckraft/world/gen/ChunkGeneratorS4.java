e7620bc8af066dfa7309002d81f7d87f706d85a4
boringbeard <boringdinotank@gmail.com>
2021-07-30 09:44:03 -0600

    doesnt work

--- src/main/java/com/boringbread/muckraft/world/gen/ChunkGeneratorS4.java
+++ src/main/java/com/boringbread/muckraft/world/gen/ChunkGeneratorS4.java
@@ -3,6 +3,7 @@ package com.boringbread.muckraft.world.gen;
 import com.dhanantry.scapeandrunparasites.init.SRPBlocks;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
@@ -17,11 +18,13 @@ import java.util.Random;
 
 public class ChunkGeneratorS4 implements IChunkGenerator
 {
-    private static final IBlockState DEFAULT_BLOCK = SRPBlocks.ParasiteRubble.getDefaultState();
+    private static final IBlockState DEFAULT_BLOCK = Blocks.STONE.getDefaultState();
     private final Random rand;
     private final World world;
     private NoiseGeneratorOctaves perlinNoise;
-    private double[] mainStructure;
+    private double[] mainStructureX;
+    private double[] mainStructureY;
+    private double[] mainStructureZ;
 
     public ChunkGeneratorS4(World worldIn, boolean p_i45637_2_, long seed)
     {
@@ -34,14 +37,20 @@ public class ChunkGeneratorS4 implements IChunkGenerator
     public Chunk generateChunk(int x, int z)
     {
         ChunkPrimer primer = new ChunkPrimer();
-        mainStructure = perlinNoise.generateNoiseOctaves(mainStructure, x * 16, 0, z * 16, 16, 256, 16, 1.0, 1.0, 1.0);
+        mainStructureX = perlinNoise.generateNoiseOctaves(mainStructureX, x * 16, 0, z * 16, 0, 256, 16, 0.25, 0.25, 0.25);
+        mainStructureY = perlinNoise.generateNoiseOctaves(mainStructureY, x * 16, 0, z * 16, 16, 0, 16, 0.25, 0.25, 0.25);
+        mainStructureZ = perlinNoise.generateNoiseOctaves(mainStructureZ, x * 16, 0, z * 16, 16, 256, 0, 0.25, 0.25, 0.25);
 
-        for (int i = 0; i < mainStructure.length; i++)
+        for (int x1 = 0; x1 < 16; x1++)
         {
-            int x1 = i / 4096;
-            int z1 = i % 4096 / 256;
-            int y1 = i % 256;
-            if (mainStructure[i] > 0.5) primer.setBlockState(x1, y1, z1, DEFAULT_BLOCK);  //noise generator fills in order of y, z, x
+            for (int z1 = 0; z1 < 16; z1++)
+            {
+                for (int y = 0; y < 256; y++)
+                {
+                    double d = mainStructureX[z1 * 256 + y] + mainStructureY[x1 * 16 + z] + mainStructureZ[x1 * 256 + y];
+                    if (d > 0) primer.setBlockState(x1, y, z1, DEFAULT_BLOCK);  //noise generator fills in order of y, z, x
+                }
+            }
         }
 
         return new Chunk(world, primer, x, z);
