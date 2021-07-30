6e4dfe1cdffaaabc2b2ce54470cc8629e51dfd21
Cheddarshark <cheddarshark@gmail.com>
2021-07-29 23:11:09 -0600

    extremely inefficiently generates porous terrain for the dimension.

--- src/main/java/com/boringbread/muckraft/world/gen/ChunkGeneratorS4.java
+++ src/main/java/com/boringbread/muckraft/world/gen/ChunkGeneratorS4.java
@@ -1,5 +1,7 @@
 package com.boringbread.muckraft.world.gen;
 
+import com.dhanantry.scapeandrunparasites.init.SRPBlocks;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -7,38 +9,61 @@ import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkPrimer;
 import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraft.world.gen.NoiseGeneratorOctaves;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Random;
 
 public class ChunkGeneratorS4 implements IChunkGenerator
 {
+    private static final IBlockState DEFAULT_BLOCK = SRPBlocks.ParasiteRubble.getDefaultState();
+    private final Random rand;
     private final World world;
+    private NoiseGeneratorOctaves perlinNoise;
+    private double[] mainStructure;
 
     public ChunkGeneratorS4(World worldIn, boolean p_i45637_2_, long seed)
     {
+        this.rand = new Random(seed);
         this.world = worldIn;
+        this.perlinNoise = new NoiseGeneratorOctaves(rand, 4);
     }
 
     @Override
     public Chunk generateChunk(int x, int z)
     {
-        return new Chunk(world, new ChunkPrimer(), x, z);
+        ChunkPrimer primer = new ChunkPrimer();
+        mainStructure = perlinNoise.generateNoiseOctaves(mainStructure, x * 16, 0, z * 16, 16, 256, 16, 1.0, 1.0, 1.0);
+
+        for (int i = 0; i < mainStructure.length; i++)
+        {
+            int x1 = i / 4096;
+            int z1 = i % 4096 / 256;
+            int y1 = i % 256;
+            if (mainStructure[i] > 0.5) primer.setBlockState(x1, y1, z1, DEFAULT_BLOCK);  //noise generator fills in order of y, z, x
+        }
+
+        return new Chunk(world, primer, x, z);
     }
 
     @Override
-    public void populate(int x, int z) {
+    public List<Biome.SpawnListEntry> getPossibleCreatures(EnumCreatureType creatureType, BlockPos pos)
+    {
+        return null;
+    }
+
+    @Override
+    public void populate(int x, int z)
+    {
 
     }
+
+    //FOCUS ABOVE FOR NOW, STRUCTURES CAN COME LATER
 
     @Override
     public boolean generateStructures(Chunk chunkIn, int x, int z) {
         return false;
-    }
-
-    @Override
-    public List<Biome.SpawnListEntry> getPossibleCreatures(EnumCreatureType creatureType, BlockPos pos) {
-        return null;
     }
 
     @Nullable
@@ -48,7 +73,8 @@ public class ChunkGeneratorS4 implements IChunkGenerator
     }
 
     @Override
-    public void recreateStructures(Chunk chunkIn, int x, int z) {
+    public void recreateStructures(Chunk chunkIn, int x, int z)
+    {
 
     }
 
