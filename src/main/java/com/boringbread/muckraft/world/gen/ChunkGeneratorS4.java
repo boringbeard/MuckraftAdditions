80a02a98c3c7f3030262d7066adc7da68c3fc792
boringbeard <boringdinotank@gmail.com>
2021-07-31 23:31:00 -0600

    added interpolation

--- src/main/java/com/boringbread/muckraft/world/gen/ChunkGeneratorS4.java
+++ src/main/java/com/boringbread/muckraft/world/gen/ChunkGeneratorS4.java
@@ -37,33 +37,53 @@ public class ChunkGeneratorS4 implements IChunkGenerator
     public Chunk generateChunk(int x, int z)
     {
         ChunkPrimer primer = new ChunkPrimer();
-        mainStructure = perlinNoise.generateNoiseOctaves(mainStructure, x * 4, 0, z * 4, 5, 17, 5, 0.25, 0.25, 0.25);
+        mainStructure = perlinNoise.generateNoiseOctaves(mainStructure, x * 4, 0, z * 4, 5, 33, 5, 0.25, 2.0, 0.25);
 
         for (int x1 = 0; x1 < 4; x1++)
         {
             for (int z1 = 0; z1 < 4; z1++)
             {
-                for (int y = 0; y < 16; y++)
+                for (int y = 0; y < 32; y++)
                 {
-                    double d = mainStructure[(x1 * 4 + z1) * 16 + y];
-                    double d1 = mainStructure[(x1 * 4 + z1) * 16 + y];
+                    double origin = mainStructure[(x1 * 4 + z1) * 32 + y];
+                    double offZ = mainStructure[(x1 * 4 + (z1 + 1)) * 32 + y];
+                    double offY = mainStructure[(x1 * 4 + z1) * 32 + y + 1];
+                    double offZY = mainStructure[(x1 * 4 + (z1 + 1)) * 32 + y + 1];
+                    double originIncX = (mainStructure[((x1 + 1) * 4 + z1) * 32 + y] - origin) * 0.25;
+                    double plusZIncX = (mainStructure[((x1 + 1) * 4 + (z1 + 1)) * 32 + y] - offZ) * 0.25;
+                    double plusYIncX = (mainStructure[((x1 + 1) * 4 + z1) * 32 + y + 1] - offY) * 0.25;
+                    double plusZYIncX = (mainStructure[((x1 + 1) * 4 + (z1 + 1)) * 32 + y + 1] - offZY) * 0.25;
 
                     for (int x2 = 0; x2 < 4; x2++)
                     {
+                        double origin1 = origin;
+                        double offY1 = offY;
+                        double originIncZ = (offZ - origin) * 0.25;
+                        double offYIncZ = (offZY - offY) * 0.25;
+
                         for (int z2 = 0; z2 < 4; z2++)
                         {
-                            for (int y1 = 0; y1 < 16; y1++)
+                            double origin2 = origin1;
+                            double originIncY = (offY1 - origin1) * 0.125;
+                            for (int y1 = 0; y1 < 8; y1++)
                             {
                                 IBlockState toFill = DEFAULT_BLOCK;
-                                if (d > 0) toFill = AIR;
+                                if (origin2 > 0) toFill = AIR;
                                 int x3 = x2 + 4 * x1;
-                                int y2 = y1 + 16 * y;
+                                int y2 = y1 + 8 * y;
                                 int z3 = z2 + 4 * z1;
-
-                                //if (y2 > 254 - this.rand.nextInt(5) || y < 1 + this.rand.nextInt(5)) toFill = BEDROCK;
+                                if (y2 > 254 - this.rand.nextInt(5) || y2 < 1 + this.rand.nextInt(5)) toFill = BEDROCK;
                                 primer.setBlockState(x3, y2, z3, toFill);  //noise generator fills in order of y, z, x
+                                origin2 += originIncY;
                             }
+
+                            origin1 += originIncZ;
+                            offY1 += offYIncZ;
                         }
+                        origin += originIncX;
+                        offZ += plusZIncX;
+                        offY += plusYIncX;
+                        offZY += plusZYIncX;
                     }
                 }
             }
