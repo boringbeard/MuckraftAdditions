8c20988759b13b4479d66e86a45bc4b409932b3c
boringbread <79241012+boringbeard@users.noreply.github.com>
2021-09-04 15:41:31 -0500

    added some mekanism ores

--- src/main/java/com/boringbread/muckraft/event/OreHandler.java
+++ src/main/java/com/boringbread/muckraft/event/OreHandler.java
@@ -1,6 +1,7 @@
 package com.boringbread.muckraft.event;
 
 import com.boringbread.muckraft.config.Config;
+import mekanism.common.MekanismBlocks;
 import net.minecraft.block.BlockStone;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
@@ -16,24 +17,18 @@ import java.util.Random;
 
 public class OreHandler
 {
-    private static final int[] S1_COUNTS = {24, 15, 1, 2, 0, 0};
-    private static final int[] S1_SIZES = {26, 7, 6, 4, 0, 0};
-    private static final int[] S2_COUNTS = {20, 20, 5, 8, 1, 1};
-    private static final int[] S2_SIZES = {17, 9, 10, 7, 6, 7};
-    private static final int[] S3_COUNTS = {22, 25, 3, 10, 2, 2};
-    private static final int[] S3_SIZES = {24, 12, 8, 10, 12, 6};
+    private static final int[] S1_COUNTS = {24, 15, 1, 2, 0, 0, 0, 0, 0};
+    private static final int[] S1_SIZES = {26, 7, 6, 4, 0, 0, 0, 0, 0};
+    private static final int[] S2_COUNTS = {20, 20, 5, 8, 1, 1, 8, 14, 16};
+    private static final int[] S2_SIZES = {17, 9, 10, 7, 6, 7, 6, 8, 8};
+    private static final int[] S3_COUNTS = {22, 25, 3, 10, 2, 2, 12, 16, 20};
+    private static final int[] S3_SIZES = {24, 12, 8, 10, 12, 6, 10, 12, 12};
     public static WorldGenerator gravelGen = new WorldGenSand(Blocks.GRAVEL, 6);
     public static WorldGenerator dirtGen = new WorldGenMinable(Blocks.DIRT.getDefaultState(), 33);
     public static WorldGenerator gravelOreGen = new WorldGenMinable(Blocks.GRAVEL.getDefaultState(), 33);
     public static WorldGenerator graniteGen = new WorldGenMinable(Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.GRANITE), 33);
     public static WorldGenerator dioriteGen = new WorldGenMinable(Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.DIORITE), 33);
     public static WorldGenerator andesiteGen = new WorldGenMinable(Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.ANDESITE), 33);
-    public static WorldGenerator coalGen;
-    public static WorldGenerator ironGen;
-    public static WorldGenerator goldGen;
-    public static WorldGenerator redstoneGen;
-    public static WorldGenerator diamondGen;
-    public static WorldGenerator lapisGen;
     private static BlockPos chunkPos;
 
     @SubscribeEvent
@@ -65,20 +60,20 @@ public class OreHandler
 
     private static void generateS1Ores(World worldIn, Random random)
     {
-        generateVanillaOres(worldIn, random, S1_COUNTS, S1_SIZES);
+        generateMuckraftOres(worldIn, random, S1_COUNTS, S1_SIZES);
     }
 
     private static void generateS2Ores(World worldIn, Random random)
     {
-        generateVanillaOres(worldIn, random, S2_COUNTS, S2_SIZES);
+        generateMuckraftOres(worldIn, random, S2_COUNTS, S2_SIZES);
     }
 
     private static void generateS3Ores(World worldIn, Random random)
     {
-        generateVanillaOres(worldIn, random, S3_COUNTS, S3_SIZES);
+        generateMuckraftOres(worldIn, random, S3_COUNTS, S3_SIZES);
     }
 
-    private static void generateVanillaOres(World worldIn, Random random, int[] counts, int[] sizes)
+    private static void generateMuckraftOres(World worldIn, Random random, int[] counts, int[] sizes)
     {
         genStandardOre1(worldIn, random, 10, dirtGen, 0, 256);
         genStandardOre1(worldIn, random, 10, gravelOreGen, 0, 256);
@@ -86,19 +81,16 @@ public class OreHandler
         genStandardOre1(worldIn, random, 10, graniteGen, 0, 80);
         genStandardOre1(worldIn, random, 10, andesiteGen, 0, 80);
 
-        coalGen = new WorldGenMinable(Blocks.COAL_ORE.getDefaultState(), sizes[0]);
-        ironGen = new WorldGenMinable(Blocks.IRON_ORE.getDefaultState(), sizes[1]);
-        goldGen = new WorldGenMinable(Blocks.GOLD_ORE.getDefaultState(), sizes[2]);
-        redstoneGen = new WorldGenMinable(Blocks.REDSTONE_ORE.getDefaultState(), sizes[3]);
-        diamondGen = new WorldGenMinable(Blocks.DIAMOND_ORE.getDefaultState(), sizes[4]);
-        lapisGen = new WorldGenMinable(Blocks.LAPIS_ORE.getDefaultState(), sizes[5]);
+        genStandardOre1(worldIn, random, counts[0], new WorldGenMinable(Blocks.COAL_ORE.getDefaultState(), sizes[0]), 0, 128);
+        genStandardOre1(worldIn, random, counts[1], new WorldGenMinable(Blocks.IRON_ORE.getDefaultState(), sizes[1]), 0, 64);
+        genStandardOre1(worldIn, random, counts[2], new WorldGenMinable(Blocks.GOLD_ORE.getDefaultState(), sizes[2]), 0, 32);
+        genStandardOre1(worldIn, random, counts[3], new WorldGenMinable(Blocks.REDSTONE_ORE.getDefaultState(), sizes[3]), 0, 16);
+        genStandardOre1(worldIn, random, counts[4], new WorldGenMinable(Blocks.DIAMOND_ORE.getDefaultState(), sizes[4]), 0, 16);
+        genStandardOre2(worldIn, random, counts[5], new WorldGenMinable(Blocks.LAPIS_ORE.getDefaultState(), sizes[5]), 16, 16);
 
-        genStandardOre1(worldIn, random, counts[0], coalGen, 0, 128);
-        genStandardOre1(worldIn, random, counts[1], ironGen, 0, 64);
-        genStandardOre1(worldIn, random, counts[2], goldGen, 0, 32);
-        genStandardOre1(worldIn, random, counts[3], redstoneGen, 0, 16);
-        genStandardOre1(worldIn, random, counts[4], diamondGen, 0, 16);
-        genStandardOre2(worldIn, random, counts[5], lapisGen, 16, 16);
+        genStandardOre1(worldIn, random, counts[6], new WorldGenMinable(MekanismBlocks.OreBlock.getStateFromMeta(0), sizes[6]), 0, 60);
+        genStandardOre1(worldIn, random, counts[7], new WorldGenMinable(MekanismBlocks.OreBlock.getStateFromMeta(1), sizes[7]), 0, 60);
+        genStandardOre1(worldIn, random, counts[8], new WorldGenMinable(MekanismBlocks.OreBlock.getStateFromMeta(2), sizes[8]), 0, 60);
     }
 
     private static void genStandardOre1(World worldIn, Random random, int blockCount, WorldGenerator generator, int minHeight, int maxHeight)
